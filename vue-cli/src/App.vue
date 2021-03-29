be4d968e18811123a0b938fd5a5bddd8c6e240c7
asd3638 <asd36384@gmail.com>
2021-03-29 17:58:53 +0900

    initial commit

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<!-- 탬플릿에서 root 태그는 딱 하나만 존재 해야 한다. -->
+<!-- 같은 계층으로 dir 여러개 만들면 안된다. -->
+  <div>
+    <app-header 
+    v-bind:propsdata="header"
+    v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+//원래 객체로 var 로 저장했던 것을 파일로 저장해서 가져왔다는 차이밖에 없다.
+import AppHeader from './components/AppHeader.vue';
+
+// new Vue 랑은 달리 컴포넌트를 재사용할 수 있다는 점이 강조됨.
+// data에 값을 직접 할당하는 건 아니고 function return으로 한번 더 거쳐감
+export default {
+  //new Vue 였을 때랑은 차이가 있음.
+  data: function() {
+    return {
+      header: 'header!',
+    }
+  },
+  components: {
+    'app-header': AppHeader,
+  },
+  methods: {
+    renewStr: function() {
+      this.header = "renewedHeader!";
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <!-- label 의 for이랑 input name 맞춰주면 label이랑 input은 한 쌍이 된다. -->
+      <label for="password">pw: </label>
+      <input type="password" id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      //submit type이 기본적으로 새로고침을 해서
+      //event 받아서 새로고침 default를 없애.
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data ={
+        username: this.username,
+        password: this.password,
+      }
+      axios.post(url, data)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
